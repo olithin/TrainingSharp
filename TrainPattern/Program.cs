4fad77d724db7b4f19f8d077eafbc0b9371acea6
aosipova <aosipova@accesssoftek.com>
2020-02-05 22:57:19 +0200

    files in folder

---
+++ TrainPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace TrainPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new DirectoryInfo(@"C:\temp");
+            FileInfo[] files = t.GetFiles();
+            foreach (var item in files)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
